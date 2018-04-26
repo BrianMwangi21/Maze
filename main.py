2447b165389f628948429b4751f96eeaf73c939b
Brian Mwangi <mwangibrian8@gmail.com>
2018-04-26 12:24:35 +0300

    Enabled argument parsing from command line

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 # Maze solver in Python
+import argparse
 from packages.maze import Maze
 from packages.solver import Solver
 
-# Create new Maze Object
+parser = argparse.ArgumentParser()
+parser.add_argument("letter", help="This is the initial letter to use")
+parser.add_argument("--trail", help="This sets the trail to true", action="store_true")
+args = parser.parse_args()
+
 if __name__ == "__main__":
+    # Create new Maze Object
     maze = Maze()
-    solver = Solver(maze, "S", True, [])
+
+    if args.trail:
+        solver = Solver(maze, args.letter, True, [] )
+    else:
+        solver = Solver(maze, args.letter, False, [] )
+
     solver.solve_maze()
     solver.print_routes()
     solver.get_shortest_route()
